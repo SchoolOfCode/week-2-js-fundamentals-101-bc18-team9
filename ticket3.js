9305d7daf492802b9fcfbd372ba6027b8c3a53a6
Maddy <184935886+Maddy-bee@users.noreply.github.com>
2024-12-02 15:39:00 +0000

    Create MVP 2
    Careate function to calculate loyalty points with purchases
    less than £20

--- ticket3.js
+++ ticket3.js
@@ -21,3 +21,23 @@ if (total_value >= 20) {
 //          total_value;
 //     }
 
+//Create a function calculateLoyaltyPoints that awards points based on the purchase amount,
+//where every dollar spent earns 1 point if no discount is applied.
+//This function aims to build a foundation for a future loyalty program.
+
+// create function calculateLoyaltyPoints
+// e.g. if 19 dollars spent 19 points earned 
+// if  20 dollars spent 0 points earned 
+
+let customer1 = 1 
+
+function calculateLoyaltyPoints (total_value) {
+    if (total_value < 20){
+        
+        return customer1 + Math.floor(total_value)
+    }
+    else  {
+        return customer1
+    }
+
+    }
