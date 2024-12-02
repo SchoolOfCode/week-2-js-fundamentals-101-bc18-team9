87c552ba3fd966d329573811d73657ae6fa752ad
“graceoreillygit config --global user.email “graceoreilly111@gmail.com <yourname@example.comf>
2024-12-02 13:26:01 +0000

    Ticket one complete

---
+++ ticket1.js
@@ -0,0 +1,10 @@
+// Write a JavaScript function `convertGBPtoEUR`
+// that takes in a specified amount in £
+// and converts it to € using a given exchange rate. 
+// The function should return the converted amount.
+// Future stretch goal - find a way of linking live exchange rates
+
+function convertGBPtoEUR (pounds, rate) {
+   let euros = pounds * rate;
+   return euros;
+}
