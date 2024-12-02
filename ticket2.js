4046059fbf072d495b1e8b755923e1eea1decba0
“graceoreillygit config --global user.email “graceoreilly111@gmail.com <yourname@example.comf>
2024-12-02 14:01:32 +0000

    Ticket 2 MVP 1

---
+++ ticket2.js
@@ -0,0 +1,8 @@
+// Develop a JavaScript function displayTemperatureInCelsius 
+// that takes in a numerical Fahrenheit temperature reading and converts it to Celsius,
+// returning the number representing the Celsius value.
+
+function displayTemperatureInCelsius (f) {
+let celcius = (f - 32) * 5/9
+return celcius;
+}
